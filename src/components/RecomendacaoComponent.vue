66af4cd8cdf4dc6fe91b0861032eee43e2ced688
Naylton <suporte@3v3.com.br>
2024-04-16 15:28:25 -0300

    Finalizando Validações

--- src/components/RecomendacaoComponent.vue
+++ src/components/RecomendacaoComponent.vue
@@ -4,36 +4,75 @@
       <div class="row">
         <div class="col-md-4">
           <div class="form-group mb-4">
-            <label for="option1" class="tab-label">Estação</label>
+            <label
+              for="option1"
+              class="tab-label"
+            >Estação</label>
             <div class="input-group">
-              <select id="option1" v-model="selectedOption1" class="form-control">
-                <option value="opcao1">Selecione a Estação</option>
-                <option value="opcao2">Maranguape (Funceme)</option>
-                <option value="opcao3">Maracanaú (Funceme)</option>
+              <select
+                id="option1"
+                v-model="selectedOption1"
+                class="form-control"
+              >
+                <option value="opcao1">
+                  Selecione a Estação
+                </option>
+                <option value="opcao2">
+                  Maranguape (Funceme)
+                </option>
+                <option value="opcao3">
+                  Maracanaú (Funceme)
+                </option>
               </select>
             </div>
           </div>
         </div>
         <div class="col-md-4">
           <div class="form-group mb-4">
-            <label for="option2" class="tab-label">Pluviômetro</label>
+            <label
+              for="option2"
+              class="tab-label"
+            >Pluviômetro</label>
             <div class="input-group">
-              <select id="option2" v-model="selectedOption2" class="form-control">
-                <option value="opcaoA">Selecione o Pluviômetro</option>
-                <option value="opcaoB">Maranguape (Funceme)</option>
-                <option value="opcaoC">Maracanaú (Funceme)</option>
+              <select
+                id="option2"
+                v-model="selectedOption2"
+                class="form-control"
+              >
+                <option value="opcaoA">
+                  Selecione o Pluviômetro
+                </option>
+                <option value="opcaoB">
+                  Maranguape (Funceme)
+                </option>
+                <option value="opcaoC">
+                  Maracanaú (Funceme)
+                </option>
               </select>
             </div>
           </div>
         </div>
         <div class="col-md-4">
           <div class="form-group mb-4">
-            <label for="option3" class="tab-label">Cultura</label>
+            <label
+              for="option3"
+              class="tab-label"
+            >Cultura</label>
             <div class="input-group">
-              <select id="option3" v-model="selectedOption3" class="form-control">
-                <option value="opcaoX">Selecione a Cultura</option>
-                <option value="opcaoY">Abacate</option>
-                <option value="opcaoZ">Acerola</option>
+              <select
+                id="option3"
+                v-model="selectedOption3"
+                class="form-control"
+              >
+                <option value="opcaoX">
+                  Selecione a Cultura
+                </option>
+                <option value="opcaoY">
+                  Abacate
+                </option>
+                <option value="opcaoZ">
+                  Acerola
+                </option>
               </select>
             </div>
           </div>
@@ -42,35 +81,75 @@
       <div class="row">
         <div class="col-md-4">
           <div class="form-group mb-4">
-            <label for="input1" class="tab-label">Data de Plantio</label>
+            <label
+              for="input1"
+              class="tab-label"
+            >Data de Plantio</label>
             <div class="input-group">
-              <input id="input1" type="date" class="form-control" placeholder="Digite a Data">
+              <input
+                id="input1"
+                type="date"
+                class="form-control"
+                placeholder="Digite a Data"
+              >
             </div>
           </div>
         </div>
         <div class="col-md-4">
           <div class="form-group mb-4">
-            <label for="option4" class="tab-label">Sistema de Irrigação</label>
+            <label
+              for="option4"
+              class="tab-label"
+            >Sistema de Irrigação</label>
             <div class="input-group">
-              <select id="option4" v-model="selectedOption4" class="form-control" @change="toggleAdditionalFields">
-                <option value="opcao1">Selecione o Sistema de Irrigação</option>
-                <option value="Aspersão">Aspersão</option>
-                <option value="MicroAspersão">Microaspersão</option>
-                <option value="Gotejamento">Gotejamento</option>
-                <option value="Pivô Central">Pivô Central</option>
-                <option value="Sulcos">Sulcos</option>
+              <select
+                id="option4"
+                v-model="selectedOption4"
+                class="form-control"
+                @change="toggleAdditionalFields"
+              >
+                <option value="opcao1">
+                  Selecione o Sistema de Irrigação
+                </option>
+                <option value="Aspersão">
+                  Aspersão
+                </option>
+                <option value="MicroAspersão">
+                  Microaspersão
+                </option>
+                <option value="Gotejamento">
+                  Gotejamento
+                </option>
+                <option value="Pivô Central">
+                  Pivô Central
+                </option>
+                <option value="Sulcos">
+                  Sulcos
+                </option>
               </select>
             </div>
           </div>
         </div>
         <div class="col-md-4">
           <div class="form-group mb-4">
-            <label for="input2" class="tab-label">Eficiência de Irrigação</label>
+            <label
+              for="input2"
+              class="tab-label"
+            >Eficiência de Irrigação</label>
             <div class="input-group">
-              <input id="input2" type="text" class="form-control" placeholder="Digite a Eficiência" :disabled="isFieldDisabled">
+              <input
+                id="input2"
+                type="text"
+                class="form-control"
+                placeholder="Digite a Eficiência"
+                :disabled="isFieldDisabled"
+              >
               <div class="input-group-append">
                 <div class="input-group-text">
-                  <input type="checkbox" v-model="useDefault"> Usar padrão
+                  <input
+                    v-model="useDefault"
+                    type="checkbox"
+                  > Usar padrão
                 </div>
               </div>
             </div>
@@ -78,74 +157,180 @@
         </div>
       </div>
       <!-- Campos adicionais -->
-      <div v-if="showAdditionalFields" class="row">
-        <div v-if="selectedOption4 === 'Aspersão'" class="col-md-4">
+      <div
+        v-if="showAdditionalFields"
+        class="row"
+      >
+        <div
+          v-if="selectedOption4 === 'Aspersão'"
+          class="col-md-4"
+        >
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Precipitação por Aspersor (mm/h)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Precipitação por Aspersor (mm/h)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
         </div>
-        <div v-if="selectedOption4 === 'MicroAspersão'" class="col-md-4">
+        <div
+          v-if="selectedOption4 === 'MicroAspersão'"
+          class="col-md-4"
+        >
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Vazão do Sistema (l/h)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Vazão do Sistema (l/h)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Área Plantada (m²)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Área Plantada (m²)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Área efetiva de cada planta (m²/planta)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Área efetiva de cada planta (m²/planta)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Número de Plantas por área (plantas/m²)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Número de Plantas por área (plantas/m²)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
         </div>
-        <div v-if="selectedOption4 === 'Gotejamento'" class="col-md-4">
+        <div
+          v-if="selectedOption4 === 'Gotejamento'"
+          class="col-md-4"
+        >
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Vazão do Sistema (l/h)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Vazão do Sistema (l/h)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Área Plantada (m²)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Área Plantada (m²)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Área efetiva de cada planta (m²/planta)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Área efetiva de cada planta (m²/planta)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Número de Plantas por área (plantas/m²)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Número de Plantas por área (plantas/m²)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
         </div>
-        <div v-if="selectedOption4 === 'Pivô Central'" class="col-md-4">
+        <div
+          v-if="selectedOption4 === 'Pivô Central'"
+          class="col-md-4"
+        >
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Precipitação por volta (mm/volta)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Precipitação por volta (mm/volta)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
         </div>
-        <div v-if="selectedOption4 === 'Sulcos'" class="col-md-4">
+        <div
+          v-if="selectedOption4 === 'Sulcos'"
+          class="col-md-4"
+        >
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Comprimento dos Sulcos (m)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Comprimento dos Sulcos (m)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Espaçamento entre os Sulcos (m)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Espaçamento entre os Sulcos (m)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
           <div class="form-group mb-4">
-            <label class="tab-label" style="color: #007bff;">Vazão por Sulco (l/h)</label>
-            <input type="text" class="form-control">
+            <label
+              class="tab-label"
+              style="color: #007bff;"
+            >Vazão por Sulco (l/h)</label>
+            <input
+              type="text"
+              class="form-control"
+            >
           </div>
         </div>
       </div>
       <div class="row">
         <div class="col-md-12">
-          <button class="btn btn-primary" @click="calcularRecomendacao">Simular Lâmina</button>
-          <button class="btn btn-danger mr-2" @click="limparCampos">Limpar</button>
+          <button
+            class="btn btn-primary"
+            @click="calcularRecomendacao"
+          >
+            Simular Lâmina
+          </button>
+          <button
+            class="btn btn-danger mr-2"
+            @click="limparCampos"
+          >
+            Limpar
+          </button>
         </div>
       </div>
     </div>
