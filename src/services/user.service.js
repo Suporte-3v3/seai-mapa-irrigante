9a363138848000e392d784ef06d45fe5ea240528
Felipe Henrique <fh27.95@gmail.com>
2024-08-19 21:40:47 -0300

    Fix: Login, Register, ChangePassword and ConfirmRegister

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -8,4 +8,17 @@ export class UserRest {
     const result = await http.post(`v1/user/irrigant`, data);
     return result.data;
   }
+  async sendLink(data) {
+    const result = await http.post(`v1/user/irrigant/forgot-password`, data);
+    return result.data;
+  }
+  async savePassword(data, code) {
+    const result = await http.post(`v1/user/irrigant/forgot-password/${code}`, data);
+    return result.data;
+  }
+  async confirmRegister(code) {
+    const result = await http.patch(`v1/user/irrigant/activate/${code}`);
+    return result.data;
+  }
+
 }
---
+++ src/views/User/Activate.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="background">
+    <div class="container-fluid login d-flex h-100">
+      <div class="login-content">
+        <TabView
+          :pt="{
+            inkbar: () => ({
+              id: 'myPanelHeader',
+              style: {
+                'background-color': '#99BDFF00',
+              },
+            }),
+          }"
+          v-model:activeIndex="activeTab"
+        >
+          <TabPanel header="Confirmação de cadastro">
+            <ConfirmRegister />
+          </TabPanel>
+        </TabView>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref} from "vue";
+import ConfirmRegister from "../../components/Register/ConfirmRegister.vue";
+
+export default {
+  name: "ResetPassword",
+  components: { ConfirmRegister },
+  setup() {
+    const activeTab = ref(0);
+    return {
+      activeTab,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.background {
+  height: 100vh;
+  background: linear-gradient(180deg, #1b3f82 0%, #112549 100%);
+}
+.login {
+  justify-content: center;
+  align-items: center;
+}
+.login-content {
+  background-color: white;
+  width: 30vw;
+  max-width: 728px;
+  min-height: 500px;
+  min-width: 360px;
+}
+</style>
