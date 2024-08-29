5746beb61c9f59dcde1bbd4cdc0f7aa1b321e721
Suporte-3v3 <126183429+Suporte-3v3@users.noreply.github.com>
2024-08-29 07:22:44 -0300

    Ajuste na hora

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -99,7 +99,7 @@ export default {
       const month = String(date.getUTCMonth() + 1).padStart(2, '0');
       const year = date.getUTCFullYear();
       const hours = String(date.getUTCHours()).padStart(2, '0');
-      return `${day}/${month}/${year} ${hours}:00 horas`;
+      return `${day}/${month}/${year}`;
     },
     setupMap() {
       
