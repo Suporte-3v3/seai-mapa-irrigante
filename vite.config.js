791fddc1944c0df87a58a3c7d81e70952eff18d3
Spinnafre <davispenha@gmail.com>
2024-10-21 16:07:47 -0300

    run using .env files

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  define:{
-    'process.env': process.env,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    define: {
+      'process.env': process.env,
     },
-  },
-  server: {
-    proxy: {
-      "/api/": {
-        target: "http://seai.3v3.farm/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        "/api/": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          secure: false
+        },
+      },
+    },
+  }
 });
